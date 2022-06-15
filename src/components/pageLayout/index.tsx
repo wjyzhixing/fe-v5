8cb2c909184ad72fc7864c1931c218f28f83fd64
jsers <xqing9024@gmail.com>
2022-06-15 15:32:12 +0800

    refactor: limit number of the rules

--- src/components/pageLayout/index.tsx
+++ src/components/pageLayout/index.tsx
@@ -26,6 +26,7 @@ import { CommonStoreState } from '@/store/commonInterface';
 import { Menu, Dropdown, Button } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import { Logout } from '@/services/login';
+import AdvancedWrap from '@/components/AdvancedWrap';
 import License from '@/pages/warning/strategy/license';
 interface IPageLayoutProps {
   icon?: ReactNode;
@@ -150,7 +151,10 @@ const PageLayout: React.FC<IPageLayoutProps> = ({ icon, title, rightArea,introIc
                   </Dropdown>
                 </div>
               )}
-              <License />
+              <AdvancedWrap>
+                <License />
+              </AdvancedWrap>
+
               {/* 文案完善了再打开 */}
               {/* <span
                 className='language'
