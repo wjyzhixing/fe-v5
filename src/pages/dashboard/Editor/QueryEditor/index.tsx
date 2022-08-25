81b43df2beacf86f1652077e33e719cab38d4b76
jsers <xqing9024@gmail.com>
2022-08-25 15:07:48 +0800

    refactor: update datasource query

--- src/pages/dashboard/Editor/QueryEditor/index.tsx
+++ src/pages/dashboard/Editor/QueryEditor/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Space, Input, Form, Select } from 'antd';
+import AdvancedWrap from '@/components/AdvancedWrap';
 import Prometheus from './Prometheus';
 import ElasticSearch from './ElasticSearch';
 import ClusterSelect from './components/ClusterSelect';
@@ -11,74 +12,76 @@ const cates = [
   },
   {
     value: 'elasticsearch',
-    label: 'ElasticSearch',
+    label: 'Elasticsearch',
   },
 ];
 
 export default function index({ chartForm }) {
   return (
     <div>
-      <Space style={{ marginBottom: 10 }}>
-        <Input.Group>
-          <span className='ant-input-group-addon'>数据源类型</span>
-          <Form.Item name='datasourceCate' noStyle initialValue='prometheus'>
-            <Select
-              style={{ minWidth: 70 }}
-              onChange={(val) => {
-                if (val === 'prometheus') {
-                  chartForm.setFieldsValue({
-                    targets: [
-                      {
-                        refId: 'A',
-                        expr: '',
-                      },
-                    ],
-                  });
-                } else if (val === 'elasticsearch') {
-                  chartForm.setFieldsValue({
-                    targets: [
-                      {
-                        refId: 'A',
-                        index: '',
-                        filters: '',
-                        query: {
-                          values: [
-                            {
-                              func: 'count',
-                            },
-                          ],
-                          date_field: '@timestamp',
-                          interval: 1,
-                          interval_unit: 'min',
+      <AdvancedWrap var='VITE_IS_QUERY_ES_DS'>
+        <Space style={{ marginBottom: 10 }}>
+          <Input.Group>
+            <span className='ant-input-group-addon'>数据源类型</span>
+            <Form.Item name='datasourceCate' noStyle initialValue='prometheus'>
+              <Select
+                style={{ minWidth: 70 }}
+                onChange={(val) => {
+                  if (val === 'prometheus') {
+                    chartForm.setFieldsValue({
+                      targets: [
+                        {
+                          refId: 'A',
+                          expr: '',
                         },
-                      },
-                    ],
-                  });
-                }
-              }}
-            >
-              {cates.map((item) => (
-                <Select.Option key={item.value} value={item.value}>
-                  {item.label}
-                </Select.Option>
-              ))}
-            </Select>
+                      ],
+                    });
+                  } else if (val === 'elasticsearch') {
+                    chartForm.setFieldsValue({
+                      targets: [
+                        {
+                          refId: 'A',
+                          index: '',
+                          filters: '',
+                          query: {
+                            values: [
+                              {
+                                func: 'count',
+                              },
+                            ],
+                            date_field: '@timestamp',
+                            interval: 1,
+                            interval_unit: 'min',
+                          },
+                        },
+                      ],
+                    });
+                  }
+                }}
+              >
+                {cates.map((item) => (
+                  <Select.Option key={item.value} value={item.value}>
+                    {item.label}
+                  </Select.Option>
+                ))}
+              </Select>
+            </Form.Item>
+          </Input.Group>
+          <Form.Item shouldUpdate={(prev, curr) => prev.datasourceCate !== curr.datasourceCate} noStyle>
+            {({ getFieldValue }) => {
+              if (getFieldValue('datasourceCate') === 'elasticsearch') {
+                return (
+                  <Input.Group>
+                    <span className='ant-input-group-addon'>集群</span>
+                    <ClusterSelect />
+                  </Input.Group>
+                );
+              }
+              return null;
+            }}
           </Form.Item>
-        </Input.Group>
-        <Form.Item shouldUpdate={(prev, curr) => prev.datasourceCate !== curr.datasourceCate} noStyle>
-          {({ getFieldValue }) => {
-            if (getFieldValue('datasourceCate') === 'elasticsearch') {
-              return (
-                <Input.Group>
-                  <span className='ant-input-group-addon'>集群</span>
-                  <ClusterSelect />
-                </Input.Group>
-              );
-            }
-            return null;
-          }}
-        </Form.Item>
-      </Space>
+        </Space>
+      </AdvancedWrap>
       <Form.Item shouldUpdate={(prev, curr) => prev.datasourceCate !== curr.datasourceCate} noStyle>
         {({ getFieldValue }) => {
           const cate = getFieldValue('datasourceCate');
